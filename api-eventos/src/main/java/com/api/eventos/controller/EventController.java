6f247b3dad5d2e89ecfb26ac968c6491a9311679
dwego <dweg0epic@outlook.com>
2025-08-27 17:21:21 -0300

    feat: implement get path for get events by init month and finish month

--- api-eventos/src/main/java/com/api/eventos/controller/EventController.java
+++ api-eventos/src/main/java/com/api/eventos/controller/EventController.java
@@ -1,12 +1,29 @@
 package com.api.eventos.controller;
 
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import com.api.eventos.model.MonthEvent;
+import com.api.eventos.service.EventService;
+import org.springframework.web.bind.annotation.*;
+
+import java.time.Month;
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/event")
 public class EventController {
 
+    private final EventService eventService;
+
+    public EventController(EventService eventService) {
+        this.eventService = eventService;
+    }
+
+    @GetMapping("/months")
+    public List<MonthEvent> getByMonth(
+            @RequestParam(name = "init_month") Month init_month,
+            @RequestParam(name = "finish_month") Month finish_month
+    ) {
+        return eventService.filterMonths(init_month, finish_month);
+    }
 
 
 }
